6b1ae384c47ed8b85ecaf80e97e82aecb7f518c9
Anton Konyk <antonkonyk.com>
2024-08-23 19:09:42 +0200

    create ProfileViewSet

--- media/views.py
+++ media/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from media.permissions import IsOwnerProfile
+from media.serializers import ProfileSerializer, ProfileImageSerializer
+from media.models import Profile
+
+
+# class ProfileViewSet(viewsets.ModelViewSet):
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
+
+class ProfileViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        permission_classes=[IsAuthenticated, IsOwnerProfile],
+        url_path="upload-image_profile",
+        serializer_class=ProfileImageSerializer,
+    )
+    def upload_image(self, request, pk=None):
+        profile = self.get_object()
+        serializer = self.get_serializer(profile, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        permission = IsOwnerProfile()
+        if not permission.has_object_permission(request, self, instance):
+            return Response({"detail": "You do not have permission to perform this action."},
+                            status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+
+        if Profile.objects.filter(user=request.user).exists():
+            return Response({"detail": "Profile already exists."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
