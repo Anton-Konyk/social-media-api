509e773f3470f87d99233a3341ef2f57bcc52cb0
Anton Konyk <antonkonyk.com>
2024-08-23 14:45:00 +0200

    add profile models to user

--- user/serializers.py
+++ user/serializers.py
@@ -2,13 +2,29 @@ from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 
+from media.models import Profile
+
 
 class UserSerializer(serializers.ModelSerializer):
-    bio = serializers.CharField(source="profile.bio")
+    profile_pic = serializers.ImageField(source="profile.profile_pic", required=False)
+    bio = serializers.CharField(source="profile.bio", required=False)
+    following = serializers.PrimaryKeyRelatedField(
+        queryset=get_user_model().objects.all(),
+        many=True,
+        required=False
+    )
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff", "bio")
+        fields = (
+            "id",
+            "email",
+            "password",
+            "is_staff",
+            "profile_pic",
+            "bio",
+            "following",
+        )
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {
             "password": {
@@ -16,20 +32,40 @@ class UserSerializer(serializers.ModelSerializer):
                 "min_length": 5,
                 "style": {"input_type": "password"},
                 "label": _("Password"),
+                "required": False
                 }
             }
 
     def create(self, validated_data):
-        return get_user_model().objects.create_user(**validated_data)
+        profile_data = validated_data.pop("profile", None)
+
+        user = get_user_model().objects.create_user(**validated_data)
+
+        profile = getattr(user, 'profile', None)
+        if not profile:
+            profile = Profile.objects.create(user=user)
+
+        if profile_data:
+            for attr, value in profile_data.items():
+                setattr(profile, attr, value)
+            profile.save()
+
+        return user
 
     def update(self, instance, validated_data):
         """Update User with encrypted password"""
         password = validated_data.pop("password", None)
+        profile_data = validated_data.pop("profile", None)
         user = super().update(instance, validated_data)
 
         if password:
             user.set_password(password)
             user.save()
+
+        if profile_data:
+            for attr, value in profile_data.items():
+                setattr(user.profile, attr, value)
+            user.profile.save()
 
         return user
 
