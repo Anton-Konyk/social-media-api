4866558c0018973e8243e9d3498fe4e1bac84106
Anton Konyk <antonkonyk.com>
2024-08-23 19:19:56 +0200

    create serializers for Profile

---
+++ media/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from media.models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        many=False,
+        slug_field="email"
+    )
+    following = serializers.SlugRelatedField(
+        read_only=True,
+        many=True,
+        slug_field="email"
+    )
+
+    class Meta:
+        model = Profile
+        fields = ("id", "user", "profile_pic", "bio", "following")
+        extra_kwargs = {"profile_pic": {"read_only": True}}
+
+
+class ProfileImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Profile
+        fields = ("id", "profile_pic")
