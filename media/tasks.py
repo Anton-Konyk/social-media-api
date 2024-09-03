db2521d32e35cd9cc7de83c3433586bb1d6b81bb
Anton Konyk <antonkonyk.com>
2024-09-03 20:48:40 +0200

    fix bug

--- media/tasks.py
+++ media/tasks.py
@@ -8,7 +8,7 @@ from media.models import Post
 @shared_task
 def publishing_post() -> None:
     current_time = datetime.now()
-    queryset = Post.objects.filter(published_date__lte=current_time)
+    queryset = Post.objects.filter(scheduled_publish_time__lte=current_time)
     for post in queryset:
         post.is_published = True
         post.save()
