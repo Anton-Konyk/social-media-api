5ddbd2ce020601bcb809da889a6b4104b1c4b04a
Anton Konyk <antonkonyk.com>
2024-09-11 13:18:50 +0200

    create logout test

--- media/tests/tests_social_madia_api.py
+++ media/tests/tests_social_madia_api.py
@@ -6,13 +6,16 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from media.models import Profile, movie_image_file_path
 from user.serializers import UserSerializer
 
 USER_URL = reverse("user:create")
 POSTS_URL = reverse("media:post-list")
+LOGOUT_URL = reverse("user:logout")
+TOKEN_REFRESH_URL = reverse("user:token_refresh")
 
 
 class ProfileCreateTests(TestCase):
@@ -66,5 +69,35 @@ class UnauthenticatedSocialMediaApiTests(TestCase):
         self.client = APIClient()
 
     def test_auth_required(self):
+        """Test that user can't be accessed without authenticated"""
         res = self.client.get(POSTS_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutViewTests(APITestCase):
+
+    def setUp(self):
+
+        self.user = get_user_model().objects.create_user(
+            email="testuser@example.com",
+            password="test_password12"
+        )
+
+        self.refresh = RefreshToken.for_user(self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.refresh.access_token}")
+
+    def test_logout_success(self):
+        """Test logout"""
+        response = self.client.post(LOGOUT_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertIn("detail", response.data)
+        self.assertEqual(response.data["detail"], "Successfully logged out.")
+
+        refresh_response = self.client.post(
+            TOKEN_REFRESH_URL,
+            {"refresh": str(self.refresh)},
+        )
+        self.assertEqual(refresh_response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(refresh_response.data["detail"], "Token is blacklisted")
